e7c6a237c93cb1431016accf4d3c779a150e389f
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 20:58:04 -0300

    introducao-API

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import MovieCard from "../components/MovieCard"
 import movies from "../data/movies.json"
 
 export default function MovieListPage() {
 
     const [search, setSearch] = useState("")
+    const[filmes,setfilmes] = useState([])
+
+ useEffect(() => {   
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=7c572a9f5b3ba776080330d23bb76e1e&language=pt-br')
+    .then(res => res.json())
+    .then(res=> setfilmes(res.results))
+    .catch(erro => console.log(erro))
+    .finally(() => console.log("Fim!"))
+ }, [])
 
     const handleSearch = (event) => {
         setSearch(event.target.value)
@@ -23,7 +32,7 @@ export default function MovieListPage() {
                 value={search}
                 onChange={handleSearch}
             />
-            <section className="flex">
+            {/* <section className="flex">
                 {
                     filmesFiltrados.length > 0 ?
 
@@ -33,6 +42,19 @@ export default function MovieListPage() {
                             ))
                         :
                         <p> Filme não encontrado</p>
+                }
+            </section> */}
+            <section>
+                {
+                    filmes.map(filme => 
+                        <>
+                        <h1>{filme.title}</h1>
+                        <img src= {`https://image.tmdb.org/t/p/w92${filme.poster_path}`}/>
+                        <img src= {`https://image.tmdb.org/t/p/w1280${filme.backdrop_path}`}/>
+                        <h1>{filme.overview}</h1>
+                        </>
+                        
+                    )
                 }
             </section>
         </>
